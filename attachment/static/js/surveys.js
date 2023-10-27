e63242a115def9f90e9dd0c9f2a67483fb463366
dvelez <danielvelezm@gmail.com>
2023-10-27 11:54:48 +0200

    Add exclusion management and FE quick fixes

--- attachment/static/js/surveys.js
+++ attachment/static/js/surveys.js
@@ -163,6 +163,7 @@ $(document).ready(function () {
     const screens = $(".screen-container");
     const nextButtons = $('button.next-button');
     const prevButtons = $('button.previous-button');
+    let firstLoad = true;
 
     let currentScreenIndex = 0;
 
@@ -187,39 +188,41 @@ $(document).ready(function () {
         if (currentScreenIndex < screens.length - 1) {
             missing_count = 0;
             first_missing = null;
-            //Check that all questions in screen have been 
-            screens.eq(currentScreenIndex).find('input, textarea, select').each(function () {
-                // if it is a radio button check if there's any option checked
-                switch ($(this).attr('type')) {
-                    case 'radio':
-                        check_radio_input(this);
-                        break;
+            //Check that all questions in screen have been
+            if (!firstLoad) {
+                screens.eq(currentScreenIndex).find('input, textarea, select').each(function () {
+                    // if it is a radio button check if there's any option checked
+                    switch ($(this).attr('type')) {
+                        case 'radio':
+                            check_radio_input(this);
+                            break;
 
-                    case 'number':
-                        check_number_input(this);
-                        break;
+                        case 'number':
+                            check_number_input(this);
+                            break;
 
-                    case 'text':
-                        check_text_input(this);
-                        break;
+                        case 'text':
+                            check_text_input(this);
+                            break;
 
-                    case 'hidden':
-                        check_hidden_input(this);
-                        break
+                        case 'hidden':
+                            check_hidden_input(this);
+                            break
 
-                    default:
-                        check_text_input(this);
-                        break;
-                }
-                if (missing_count > 0) {
-                    return false;
-                }
-                if (check_exclusion(this)) {
-                    // submit form
-                    $('form').submit();
-                    return false;
-                }
-            });
+                        default:
+                            check_text_input(this);
+                            break;
+                    }
+                    if (missing_count > 0) {
+                        return false;
+                    }
+                    if (check_exclusion(this)) {
+                        // submit form
+                        $('form').submit();
+                        return false;
+                    }
+                });
+            }
             if (missing_count > 0) {
                 first_missing.get(0).scrollIntoView({ behavior: 'smooth' });
                 return false;
